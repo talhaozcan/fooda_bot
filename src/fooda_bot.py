dc8a98d488672f893916cb2b8c6a949d9f2bd3ed
Nicole Julian <nicolevjulian@gmail.com>
2018-02-26 21:55:30 -0500

    WIP: fooda_bot.py, which unfortunately seems to require that we log in

--- src/fooda_bot.py
+++ src/fooda_bot.py
@@ -1,14 +1,27 @@
-import urllib2
 import requests
+import urllib2
 
-from lxml import html
 from bs4 import BeautifulSoup
-from fooda_creds import FOODA_EMAIL, FOODA_PW, CSRF_TOKEN
+from fooda_creds import FOODA_EMAIL, FOODA_PW
+from lxml import html
 
-login_dict = {
+login_url = 'https://app.fooda.com/login'
+session = requests.session()
+
+# Get authenticity token for login.
+# http://kazuar.github.io/scraping-tutorial/ is super helpful.
+result = session.get(login_url)
+tree = html.fromstring(result.text)
+csrf_token = list(set(tree.xpath('//meta[@name="csrf-token"]/@content')))[0]
+
+# Prepare login data, including token friend.
+login_payload = {
     'user[email]': FOODA_EMAIL,
     'user[password]': FOODA_PW,
-    'csrf-token': CSRF_TOKEN,
+    'csrf-token': csrf_token,
 }
 
-session_requests = requests.session()
+# Do teh login.
+result = session.post(
+    login_url, data=login_payload, headers=dict(referer=login_url))
+import pdb; pdb.set_trace()    
