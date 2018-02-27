b6766b6f965820fdaec63bee7927ba077c0ed47c
Nicole Julian <nicolevjulian@gmail.com>
2018-02-26 21:44:36 -0500

    Hopefully the correct git ignorance, plus start a python

---
+++ src/fooda_scape.py
@@ -0,0 +1,12 @@
+import urllib2
+import requests
+
+from lxml import html
+from bs4 import BeautifulSoup
+from fooda_creds import FOODA_EMAIL, FOODA_PW
+
+login_dict = {
+
+}
+
+import pdb; pdb.set_trace()    
