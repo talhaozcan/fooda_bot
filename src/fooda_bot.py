fef12e9f28a285e9763a489fd98d6eab9f62c3e0
Nicole Julian <nicolevjulian@gmail.com>
2018-03-28 19:31:16 -0400

    Successfully log in to Fooda

--- src/fooda_bot.py
+++ src/fooda_bot.py
@@ -1,27 +1,28 @@
 import requests
-import urllib2
 
 from bs4 import BeautifulSoup
 from fooda_creds import FOODA_EMAIL, FOODA_PW
 from lxml import html
 
-login_url = 'https://app.fooda.com/login'
+login_url = 'https://app.fooda.com/create'
+referrer_url = 'https://app.fooda.com/login'
 session = requests.session()
 
 # Get authenticity token for login.
 # http://kazuar.github.io/scraping-tutorial/ is super helpful.
 result = session.get(login_url)
 tree = html.fromstring(result.text)
-csrf_token = list(set(tree.xpath('//meta[@name="csrf-token"]/@content')))[0]
+auth_token = list(set(tree.xpath('//meta[@name="csrf-token"]/@content')))[0]
 
 # Prepare login data, including token friend.
 login_payload = {
     'user[email]': FOODA_EMAIL,
     'user[password]': FOODA_PW,
-    'csrf-token': csrf_token,
+    'authenticity_token': auth_token,
 }
 
 # Do teh login.
 result = session.post(
-    login_url, data=login_payload, headers=dict(referer=login_url))
+    login_url, data=login_payload, headers=dict(referer=referrer_url))
+
 import pdb; pdb.set_trace()    
