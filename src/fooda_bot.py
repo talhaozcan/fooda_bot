adb24d31dc8de527a163c41fcf22e00e3dbb385c
Jeremy Lavergne <jlavergne@shoobx.com>
2019-03-18 11:32:24 -0400

    update spring schedule for 150CPD

--- src/fooda_bot.py
+++ src/fooda_bot.py
@@ -94,10 +94,10 @@ def gather_food_trucks():
     """These are sorta hard-coded since there's no online source"""
     context = [
         ("Little Blue Bakery", "Teri-yummy", "Zaaki Mediterranean", "Roadie's Diner",),
-        ("Little Blue Bakery", "Kebabish", "Bon Me"),
-        ("Little Blue Bakery", "Chicken & Rice Guys", "Chicken on the Road"),
+        ("Little Blue Bakery", "Kebabish", "Gogi on the Block", "The Bacon Truck"),
+        ("Little Blue Bakery", "Chicken & Rice Guys", "Chicken on the Road", "SA PA"),
         ("Little Blue Bakery", "Sate", "The Pull-up", "North East of the Border", "Moyzilla"),
-        ("Little Blue Bakery", "Compliments", "Rhythm n' Wraps",),
+        ("Little Blue Bakery", "Zinnekens", "Compliments", "Rhythm n' Wraps",),
 
     ]
     weekday_number = datetime.datetime.today().weekday()
